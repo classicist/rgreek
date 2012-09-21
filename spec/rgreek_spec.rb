2cf1fef2505395d9e40bfb0601c3b01f9735fb55
Paul Saieg <classicist@gmail.com>
2012-09-21 10:55:26 -0600

    updated database to be more user friendly and dropped arabic parses; hit cunundrum of bad mapping between lemma_id and lemma (le/gw matches sulle/gw) - not sure what to do

--- spec/rgreek_spec.rb
+++ spec/rgreek_spec.rb
@@ -6,4 +6,11 @@ describe "rGreek" do
     Lemma.new.should_not be_nil
     Parse.new.should_not be_nil    
   end
+  
+  it "should get all parses for a lemma" do
+    lemma = Lemma.find_by_headword "kai/"
+    parses = Parse.find_all_by_lemma_id lemma.id
+    puts parses.to_yaml
+    parses.length.should == 1
+  end
 end
