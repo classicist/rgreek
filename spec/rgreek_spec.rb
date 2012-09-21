f9e808eaf533a7e804959763cdc50f94befedf46
Paul Saieg <classicist@gmail.com>
2012-09-21 10:13:55 -0600

    connected active record classes to db

--- spec/rgreek_spec.rb
+++ spec/rgreek_spec.rb
@@ -2,7 +2,8 @@
 require "spec_helper"
 
 describe "rGreek" do
-  it "should do something" do
+  it "should do exist" do
     Lemma.new.should_not be_nil
+    Parse.new.should_not be_nil    
   end
 end
