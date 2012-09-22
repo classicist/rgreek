7f5bc3e064bcd9e8563f27a300ca89f1c850d4e6
Paul Saieg <classicist@gmail.com>
2012-09-22 05:42:32 -0600

    should create a parse report for a word form

--- lib/rGreek.rb
+++ lib/rGreek.rb
@@ -43,11 +43,23 @@ module RGreek
   end
   
   class ParseReport
-    attr_reader :lemma, :parse
+    attr_reader :lemma, :parses
+    
+    def initialize(lemma, parses = [])
+      @lemma  = lemma
+      @parses = parses
+    end
     
     def self.generate(word)
-      @parses = Parse.find_parses(word)
-        
+      @parses = Parse.find_parses_hashed_by_lemma(word)
+      @parses.inject([]) do |reports, (lemma, parses)|
+        report = ParseReport.new(lemma, parses)
+        reports << report
+      end
+    end
+    
+    def to_s
+      "#{lemma.headword}: #{lemma.short_def}" + parses.map { |parse| ", #{parse.morph_code_pretty}" }.join("\n").chomp
     end
   end
 end
