26ea14d094f3ff53a6a67176932b9a5e1d9bd38e
Paul Saieg <classicist@gmail.com>
2012-09-30 04:00:33 -0600

    added specs for cleaning up lsj data

--- lib/rgreek/lemma.rb
+++ lib/rgreek/lemma.rb
@@ -1,5 +1,7 @@
 module RGreek
 class Lemma < ActiveRecord::Base
-  
+  def to_s
+    "#{id}: #{greek_headword} : #{headword} : #{short_def}" 
+  end
 end#EOC
 end#EOM
--- spec/lsj_entry_spec.rb
+++ spec/lsj_entry_spec.rb
@@ -2,7 +2,18 @@
 require 'spec_helper'
 
 describe "LsjEntry headwords" do
+  it "should be matched by every Lemma's greek_headword (there are about twice as may LsjEntries as Lemmas)" do
+    pending
+    Lemma.find(:all).each { |l| LsjEntry.find_by_headword(l.greek_headword).should_not be_nil, l}
+  end
+
+  it "should not contain a tonos" do
+    pending
+    LsjEntry.find(:all).each { |e| e.headword.should_not contain_tonos}
+  end
+
   it "should find an entry with greek copied from db betacode (check for encoding issues)" do
+    pending
     greek_copied_from_db = "καί"
     LsjEntry.find_by_headword(greek_copied_from_db).headword.should == greek_copied_from_db
   end
