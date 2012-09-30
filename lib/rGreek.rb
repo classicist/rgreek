680e00b19e3a11327e7f318c47af8c7a4e563d11
Paul Saieg <classicist@gmail.com>
2012-09-30 03:03:33 -0600

    moved database to postgres form sqlite for speed

--- lib/rGreek.rb
+++ lib/rGreek.rb
@@ -2,6 +2,6 @@ require "active_record"
 Dir[File.join(File.dirname(__FILE__), 'rgreek', '*.rb')].each { |file| require file.gsub(".rb", "")}
 
 module RGreek
-  database_name = ENV["RGREEK_ENV"] == "test" ? "rgreek_test.sqlite" : "rgreek.sqlite"
-  ActiveRecord::Base.establish_connection adapter: "sqlite3", database: "lib/data/db/#{database_name}"
+  database_name = ENV["RGREEK_ENV"] == "test" ? "rgreek_test" : "rgreek"
+  ActiveRecord::Base.establish_connection adapter: "postgresql", encoding: "unicode", database: database_name, pool: 5, username: "paul", password: ""
 end
