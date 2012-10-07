ee89e29ae5043ad75c451168c80cfe184bfe08f6
Paul Saieg <classicist@gmail.com>
2012-10-07 00:41:19 -0600

    moved off tests of db integrity into non-running spec

--- spec/db_integrity_wishlist_spec.rb
+++ spec/db_integrity_wishlist_spec.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 require 'spec_helper'
 
-describe "LsjEntry headwords" do
+describe "Database Integrity Wishlist Checks" do
 #  it "should be matched by every Lemma's unicode_headword (there are about twice as may LsjEntries as Lemmas)" do
 #    pending
 #    Lemma.find(:all).each { |l| LsjEntry.find_by_headword(l.unicode_headword).should_not be_nil, l}
@@ -19,5 +19,21 @@ describe "LsjEntry headwords" do
 #    entry = LsjEntry.find_by_headword(typed_greek)
 #    entry.should_not == nil
 #    entry.headword.should == typed_greek    
+#  end
+#  it "should have at least one parse" do
+#    pending
+#    #have to do this in sql bc of speed; 
+#    #there are 4218 unparsed Lemmas in the DB
+#    #@all_greek_lemmas.map { |l| l.parses.length == 0}.length.should == 0
+#  end
+#  
+#  it "should have a short_def if it is a greek word" do
+#    pending
+#    # 2142 greek lemmas fail this test 
+#  end
+#  
+#  it "should have at least one lsj_entry" do
+#    pending
+#    #3078 lemmas have no lsj entry
 #  end
 end
--- spec/lemma_spec.rb
+++ spec/lemma_spec.rb
@@ -16,21 +16,4 @@ describe "Lemma" do
     parses = Parse.find_all_by_lemma_id lemma.id
     parses.length.should == 5
   end
-  
-  it "should have at least one parse" do
-    pending
-    #have to do this in sql bc of speed; 
-    #there are 4218 unparsed Lemmas in the DB
-    #@all_greek_lemmas.map { |l| l.parses.length == 0}.length.should == 0
-  end
-  
-  it "should have a short_def if it is a greek word" do
-    pending
-    # 2142 greek lemmas fail this test 
-  end
-  
-  it "should have at least one lsj_entry" do
-    pending
-    #3078 lemmas have no lsj entry
-  end
 end
