a7fb0fb276703240db2fe2df4d0d14cb6f62d14b
Paul Saieg <classicist@gmail.com>
2012-10-07 01:14:23 -0600

    moved classes into more organized folder structure

--- lib/rGreek.rb
+++ lib/rGreek.rb
@@ -1,5 +1,5 @@
 require "active_record"
-Dir[File.join(File.dirname(__FILE__), 'rgreek', '*.rb')].each { |file| require file.gsub(".rb", "")}
+Dir[File.join(File.dirname(__FILE__), 'rgreek', '**/*.rb')].each { |file| require file.gsub(".rb", "")}
 
 module RGreek
   database_name = ENV["RGREEK_ENV"] == "test" ? "rgreek_test" : "rgreek"
