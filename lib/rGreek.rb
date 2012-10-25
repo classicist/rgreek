e0658e1b20e5bf7ab086c02eddbf808aeda1a4ba
Paul Saieg <classicist@gmail.com>
2012-10-25 02:48:33 -0600

    moved database dependent code into rails app so that gem can just be a gem

--- lib/rGreek.rb
+++ lib/rGreek.rb
@@ -2,6 +2,4 @@ require "active_record"
 Dir[File.join(File.dirname(__FILE__), 'rgreek', '**/*.rb')].each { |file| require file.gsub(".rb", "")}
 
 module RGreek
-  database_name = ENV["RGREEK_ENV"] == "test" ? "rgreek_test" : "rgreek"
-  ActiveRecord::Base.establish_connection adapter: "postgresql", encoding: "unicode", database: database_name, pool: 5, username: "paul", password: ""
 end
